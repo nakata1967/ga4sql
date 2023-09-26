640a068ae53c22a9d8d82003c680e44917656897
Yoshihiko Nakata <naka.sql.note@gmail.com>
2023-09-26 19:47:50 +0900

    ver.2

--- ss.sql
+++ ss.sql
@@ -1,14 +1,34 @@
-WITH page_sessions AS (
+WITH
+  page_sessions AS (
   SELECT
-    (SELECT value.string_value FROM UNNEST(event_params) WHERE event_name = 'page_view' AND key = 'page_location') AS page_location,
-    CONCAT(user_pseudo_id, CAST((SELECT value.int_value FROM UNNEST(event_params) WHERE key = 'ga_session_id') AS STRING)) AS session_id
+    (
+    SELECT
+      value.string_value
+    FROM
+      UNNEST(event_params)
+    WHERE
+      event_name = 'page_view'
+      AND KEY = 'page_location') AS page_location,
+    CONCAT(user_pseudo_id, CAST((
+        SELECT
+          value.int_value
+        FROM
+          UNNEST(event_params)
+        WHERE
+          KEY = 'ga_session_id') AS STRING)) AS session_id
   FROM
     `bigquery-public-data.ga4_obfuscated_sample_ecommerce.events_*`
   WHERE
-    _TABLE_SUFFIX BETWEEN '20201101' AND '20201110'
-    AND (SELECT value.string_value FROM UNNEST(event_params) WHERE event_name = 'page_view' AND key = 'page_location') IS NOT NULL
-)
-
+    _TABLE_SUFFIX BETWEEN '20201101'
+    AND '20201110'
+    AND (
+    SELECT
+      value.string_value
+    FROM
+      UNNEST(event_params)
+    WHERE
+      event_name = 'page_view'
+      AND KEY = 'page_location') IS NOT NULL )
 SELECT
   page_location,
   COUNT(DISTINCT session_id) AS sessions
@@ -20,4 +40,5 @@ GROUP BY
   page_location
 ORDER BY
   sessions DESC
-LIMIT 10;
+LIMIT
+  10;
